f708f1973f95d0e3f7dadae99a17663f83e3fbaf
Kenref <107287396+Kenref@users.noreply.github.com>
2023-07-06 13:09:38 +1000

    added function to populate main container

--- main.js
+++ main.js
@@ -1,34 +1,46 @@
-import { getWeather, getForecast } from "./weather-module.js"
+import { forecast } from "./weather-module.js"
 
 const container = document.getElementById("main-container");
 const form = document.querySelector("form")
 
+async function getForecast(locationCall) {
+	const forecastResponse = await forecast(locationCall)
+	return {
+		area: forecastResponse.location.tz_id,
+		condition: forecastResponse.current.condition.text,
+		//code for condition 1003
+		icon: forecastResponse.current.condition.icon,
+		rain: forecastResponse.current.precip_mm,
+
+		maxTemp: forecastResponse.forecast.forecastday[0].day.maxtemp_c,
+		minTemp: forecastResponse.forecast.forecastday[0].day.mintemp_c,
+	};
+}
+
 
 form.addEventListener("submit", function (e) {
+	const icon = document.getElementById("icon")
+	const location = document.getElementById("location")
+	const condition = document.getElementById("condition")
+	const rain = document.getElementById("rain")
+	const minTemp = document.getElementById("min") 
+	const maxTemp = document.getElementById("max")
+
+
+
 	const userInput = document.getElementById("user-input");
 
-	e.preventDefault()
-	
-	let weatherLocationCall = userInput.value
-	console.log(weatherLocationCall)
-
-	getWeather(weatherLocationCall)
-		.then(weather => {
-			console.log(weather)
-		})
-})
-
-
-
-
-
-// let weatherLocationCall = "sydney"
-
-// let currentWeather = await getWeather(weatherLocationCall)
-// let forecast = await getForecast(weatherLocationCall)
-// let location = currentWeather.location.name;
-// let region = currentWeather.location.region;
-// let tempC = currentWeather.current.temp_c;
-// let tempF = currentWeather.current.temp_f;
-
-// console.log(tempC)
+	e.preventDefault();
+	let locationCall = userInput.value;
+	// let temperature = getForecast(locationCall)
+	// location.textContent = temperature
+	 getForecast(locationCall)
+		.then((response) => {
+			location.textContent = response.area
+			icon.src = response.icon
+			condition.textContent = response.condition
+			rain.textContent = response.rain + "mm"
+			minTemp.textContent = response.minTemp + "°C";
+			maxTemp.textContent = response.maxTemp + "°C";
+	});
+});
--- weather-module.js
+++ weather-module.js
@@ -1,31 +1,27 @@
 const apiKey = "6dd7ef15a71c48fcac421427230507";
 
 
-async function getWeather(location) {
-	try {
-		const response = await fetch(
-			`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}`,
-			{ mode: "cors" }
-        );
-        
-        if (!response.ok) {
-            throw new Error("failed to fetch weather data")
-        }
+// async function getWeather(location) {
+// 	try {
+// 		const response = await fetch(
+// 			`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}`,
+// 			{ mode: "cors" }
+//         );
 
-		const weather = await response.json();
-		return weather
-	} catch (error) {
-		console.log("there was an error");
-	}
-}
+// 		const weather = await response.json();
+// 		return weather
+// 	} catch (error) {
+// 		console.log("there was an errors");
+// 	}
+// }
 
-async function getForecast(location) {
+async function forecast(location) {
 	try {
 		const response = await fetch(
 			`https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${location}`,
 			{ mode: "cors" }
-		);
-		const forecast = response.json();
+        );
+        const forecast = await response.json();
 		return forecast;
 	} catch (error) {
 		console.log(error);
@@ -33,4 +29,4 @@ async function getForecast(location) {
 }
 
 
-export {getWeather, getForecast}
+export {forecast}
