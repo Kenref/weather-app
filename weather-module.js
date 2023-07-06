41fe246f2cecffb9ee332e4ec62a39765c9c3c4c
Kenref <107287396+Kenref@users.noreply.github.com>
2023-07-06 13:26:33 +1000

    added some styling

--- weather-module.js
+++ weather-module.js
@@ -1,20 +1,5 @@
 const apiKey = "6dd7ef15a71c48fcac421427230507";
 
-
-// async function getWeather(location) {
-// 	try {
-// 		const response = await fetch(
-// 			`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}`,
-// 			{ mode: "cors" }
-//         );
-
-// 		const weather = await response.json();
-// 		return weather
-// 	} catch (error) {
-// 		console.log("there was an errors");
-// 	}
-// }
-
 async function forecast(location) {
 	try {
 		const response = await fetch(
