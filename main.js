4f6f805ba084e89141d69033d7fdd429b27e8c23
Kenref <107287396+Kenref@users.noreply.github.com>
2023-10-12 11:11:59 +1100

    formatted files

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-import { forecast } from "./weather-module.js"
+import { forecast } from "./weather-module.js";
 
 const container = document.getElementById("main-container");
-const form = document.querySelector("form")
+const form = document.querySelector("form");
 
 async function getForecast(locationCall) {
-	const forecastResponse = await forecast(locationCall)
+	const forecastResponse = await forecast(locationCall);
 	return {
 		area: forecastResponse.location.tz_id,
 		condition: forecastResponse.current.condition.text,
@@ -17,30 +17,24 @@ async function getForecast(locationCall) {
 	};
 }
 
-
 form.addEventListener("submit", function (e) {
-	const icon = document.getElementById("icon")
-	const location = document.getElementById("location")
-	const condition = document.getElementById("condition")
-	const rain = document.getElementById("rain")
-	const minTemp = document.getElementById("min") 
-	const maxTemp = document.getElementById("max")
-
-
-
+	const icon = document.getElementById("icon");
+	const location = document.getElementById("location");
+	const condition = document.getElementById("condition");
+	const rain = document.getElementById("rain");
+	const minTemp = document.getElementById("min");
+	const maxTemp = document.getElementById("max");
 	const userInput = document.getElementById("user-input");
 
 	e.preventDefault();
 	let locationCall = userInput.value;
-	// let temperature = getForecast(locationCall)
-	// location.textContent = temperature
-	 getForecast(locationCall)
-		.then((response) => {
-			location.textContent = response.area
-			icon.src = response.icon
-			condition.textContent = response.condition
-			rain.textContent = response.rain + "mm"
-			minTemp.textContent = response.minTemp + "°C";
-			maxTemp.textContent = response.maxTemp + "°C";
+
+	getForecast(locationCall).then((response) => {
+		location.textContent = response.area;
+		icon.src = response.icon;
+		condition.textContent = response.condition;
+		rain.textContent = response.rain + "mm";
+		minTemp.textContent = response.minTemp + "°C";
+		maxTemp.textContent = response.maxTemp + "°C";
 	});
 });
